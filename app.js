85a951b1e5696724c439bd4448f9ecbba734100c
Tripp Lucas <tripppdx@gmail.com>
2022-02-11 14:50:22 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const facilities = JSON.parse(
+  fs.readFileSync(`${__dirname}/./dev-data/data/avalon.json`)
+);
+
+app.get('/api/v1/services/:id', (req, res) => {
+  const id = req.params.id;
+  const facility = facilities.find(el => el._id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      facility,
+    },
+  });
+});
+
+app.get('*', (_, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: "These aren't the droids you're looking for.",
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
